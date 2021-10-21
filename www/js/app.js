d855e6e79e42c99edaa21d86eab125480bea96b9
marchrys <mar.chrysikopoulos@gmail.com>
2021-10-21 17:25:42 +0900

    Remplacement du module app par une instance de Vue.

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,31 @@
-// This is a JavaScript file
-// App module
-let app = {
-  init: function(){
+const app = new Vue({
+  el: '#app',
+  data: {
+    lang: null
+  },
+  methods: {
+    detectNavigatorLanguage: function() {
+      const langStr = window.navigator.language;
+      //On détecte la langue de l'appareil
+      if(langStr.substr(0, 2) == 'fr'){
+          this.lang = 'fr';
+      }
+      else{
+          this.lang = 'en';
+      }
+    },
+    initTabs: function() {
+      let elem = document.querySelector('.tabs'); 
+      let instance = M.Tabs.init(elem, {});
+    }
+  },
+  beforeMount(){
     initSounds();
-
-     const vue = new Vue({
-       el: '#app',
-       data: {
-         lang: null,
-         message: 'Hello world!',
-         allSoundsLoaded
-       },
-       methods: {
-        //  sayHello: function() {
-        //    alert('hello!');
-        //  }
-       },
-       beforeMount(){
-          const langStr = window.navigator.language;
-          //On détecte la langue de l'appareil
-          if(langStr.substr(0, 2) == 'fr'){
-              this.lang = 'fr';
-          }
-          else{
-              this.lang = 'en';
-          }
-       },
-     });
+    this.detectNavigatorLanguage();
+  },
+  mounted(){
+    this.initTabs();
   }
-}
-
-document.addEventListener("deviceready", app.init, false);
+});
+   
+ 
---
+++ www/js/screens.js
@@ -0,0 +1,17 @@
+const screens = [
+  {
+    id: 1,
+    title: {
+      en: 'Training',
+      fr: 'Entraînement'
+    }
+  },
+  {
+    id: 2,
+    title: {
+      en: 'Settings',
+      fr: 'Options'
+    }
+  }
+];
+
